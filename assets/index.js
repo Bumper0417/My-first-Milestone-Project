bf1c37a950ccf9c27c1f4548bdcafcf9ac489e82
Christos Gkoros <chrisgoros13@gmail.com>
2019-03-07 17:20:05 +0000

    added README.md file

--- assets/index.js
+++ assets/index.js
@@ -1,21 +1,17 @@
-/*// Delayed scrolling to different sections of the page
-// Modified from a tutorial found at https://github.com/Code-Institute-Solutions/StudentExampleProjectGradeFive/blob/master/assets/js/index.js*/
-/*global global$*/
- global$('a[href^="#"]').on('click', function(event) {
+'$'('a[href^="#"]').on('click', function (event) {
 
-    var target = global$(this.getAttribute('href'));
-
-    if( target.length ) {
+    var target = '$'(this.getAttribute('href'));
+    if(target.length) {
         event.preventDefault();
-        global$('html, body').stop().animate({
+    '$'('html, body').stop().animate({
             scrollTop: target.offset().top
         }, 1000);
     }
 
 });
 
-global$('#contact-form').on('submit', function(e) {
-    global$('#contactFormModal').modal('show');
+   '$'('#contact-form').on('submit', function(e) {
+    '$'('#contactFormModal').modal('show');
     e.preventDefault();
 })
  
